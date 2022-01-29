f8ab8835647762b89b3e37d4309cdecfe12c7fe8
Michael Stipsits <michael.stipists@gmx.at>
2022-01-29 09:21:33 +0100

    inital commit

---
+++ src/main/java/at/asd/toilettinder/ToiletTinderApplication.java
@@ -0,0 +1,13 @@
+package at.asd.toilettinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToiletTinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToiletTinderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/asd/toilettinder/ToiletTinderApplicationTests.java
@@ -0,0 +1,13 @@
+package at.asd.toilettinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToiletTinderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
