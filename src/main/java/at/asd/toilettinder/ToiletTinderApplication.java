4cefb2df3670e2756abce5c0bd2ecbf175e0aeac
Mohammad Alsalkini <eng.salkini@hotmail.com>
2022-01-29 20:56:42 +0100

    Update

--- src/main/java/at/asd/toilettinder/ToiletTinderApplication.java
+++ src/main/java/at/asd/toilettinder/ToiletTinderApplication.java
@@ -10,6 +10,5 @@ import org.springframework.context.annotation.Configuration;
 public class ToiletTinderApplication {
     public static void main(String[] args) {
         SpringApplication.run(ToiletTinderApplication.class, args);
-
     }
 }
