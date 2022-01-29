f9d5c82a58ea1975099ef4134cc27b5d6bfe7fd8
Mohammad Alsalkini <eng.salkini@hotmail.com>
2022-01-29 12:01:51 +0100

    Rename rest Directory to resource

--- src/main/java/at/asd/toilettinder/resource/ToiletResource.java
+++ src/main/java/at/asd/toilettinder/resource/ToiletResource.java
@@ -1,7 +1,6 @@
-package at.asd.toilettinder.rest;
+package at.asd.toilettinder.resource;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
